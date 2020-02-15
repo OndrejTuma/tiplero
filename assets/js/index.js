9cd7ae7b35f8799f63cd6bb5858b0e015385e58f
Ondřej Tůma <ondr@centrum.cz>
2020-02-15 20:28:41 +0100

    openers, header search

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,14 +2,19 @@ const classes = {
     header: {
         active: 'header--menu-open',
     },
+    openers: {
+        active: 'opener--active',
+        targetActive: 'opener__target--active',
+        opener: 'opener',
+    },
     tabs: {
         active: 'tabs__tab--active',
         tab: 'tabs__tab',
         tabs: 'tabs',
-    }
+    },
 };
 
-const Tabs = function(classes) {
+const Tabs = function (classes) {
     this.classes = classes;
 };
 Tabs.prototype.registerListeners = function (ctx) {
@@ -43,16 +48,16 @@ const Header = function (header, classes) {
     this.headerElm = header;
     this.isOpen = false;
 };
-Header.prototype._click = function(e) {
+Header.prototype._click = function (e) {
     e.preventDefault();
 
     this.isOpen ? this._closeMenu() : this._openMenu();
 };
-Header.prototype._openMenu = function() {
+Header.prototype._openMenu = function () {
     this.isOpen = true;
     this.headerElm.classList.add(this.classes.active);
 };
-Header.prototype._closeMenu = function() {
+Header.prototype._closeMenu = function () {
     this.isOpen = false;
     this.headerElm.classList.remove(this.classes.active);
 };
@@ -61,10 +66,47 @@ Header.prototype.registerListener = function (hamburger) {
 };
 
 
-(function(w, d) {
+const Openers = function (classes) {
+    this.classes = classes;
+    this.targetDatasetAttr = 'target';
+};
+Openers.prototype.registerListeners = function (ctx) {
+    this.ctx = ctx;
+
+    ctx.addEventListener('click', this._click.bind(this));
+};
+Openers.prototype._click = function (e) {
+    if (!e.target.classList.contains(this.classes.opener)) {
+        return;
+    }
+
+    const target = this.ctx.getElementById(e.target.dataset[this.targetDatasetAttr]);
+
+    if (!target) {
+        return;
+    }
+
+    target.classList.contains(this.classes.targetActive)
+        ? this.close(e.target, target)
+        : this.open(e.target, target);
+};
+Openers.prototype.open = function (opener, target) {
+    opener.classList.add(this.classes.active);
+    target.classList.add(this.classes.targetActive);
+};
+Openers.prototype.close = function (opener, target) {
+    opener.classList.remove(this.classes.active);
+    target.classList.remove(this.classes.targetActive);
+};
+
+
+(function (w, d) {
     // init of tabs on page
     const tabs = new Tabs(classes.tabs);
     tabs.registerListeners(d);
+    // init openers on page
+    const openers = new Openers(classes.openers);
+    openers.registerListeners(d);
 
     const header = new Header(d.getElementById('header'), classes.header);
     header.registerListener(d.getElementById('hamburger'));
